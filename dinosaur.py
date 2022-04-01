043d4b0992ce4f9450d9edd2fbb25e989d6f05b1
Dyshine Hill <eyedolarts@gmail.com>
2022-03-31 23:17:15 -0500

    Fixed bug with robot attack function + added feature to robot class for random weapon + Dinos attack bug fixed

--- dinosaur.py
+++ dinosaur.py
@@ -8,7 +8,7 @@ from re import A
 
 
 class Dinosaur:
-    def __init__(self,attack_power:int,name:str,health:int) :
+    def __init__(self,name:str,health:int) :
         self.name = name
         self.attack_power = attack_power
         self.health = 100
@@ -17,17 +17,16 @@ class Dinosaur:
 
 
 
-    def attack(self,robot:list):
+    def attack(self,robot):
+        
         damages_list =[20,30,40,10,50,60]
-
         move_list = ["Bite",'Roar',"Tailwhip","Stomp"]
         self.move=choice(move_list)
-
         self.attack_power = choice(damages_list)
-
+        robot.health-=self.attack(robot)
+        
         print(f"{self.name} used {self.move}  doing {self.attack_power} points of damage ")
-        robot.health = robot.health - self.attack_power
-        print(robot.health)
+        
 
 
 
--- robot.py
+++ robot.py
@@ -1,43 +1,25 @@
-from dinosaur import Dinosaur
 from weapon import Weapon
-
-
-import random
+from random import choice
 
 #Weapon  passed in for attack_power 
 class Robot:
-    def __init__(self,name:str,Weapon,health:int) :
+    def __init__(self,name:str,health:int) :
         self.name = name
-        #Weapon.attack_power for attack power and Weapon.name to name
-        
+        self.weapon = ''  
         self.health = 100
-        self.hit_points = ''
+        self.weapon_picker()
     
-    
-    #dinosaur will be a index position and will be deleted once health <=0
+    def weapon_picker (self):
+      axe =  Weapon('axe', 20)
+      laser = Weapon(laser, 50)
+      bat = Weapon(bat, 10)
+      weed_eater = Weapon(weed_eater, 100)
+      weapon_list = [axe,laser,bat,weed_eater]
+      self.weapon = choice(weapon_list)
+
+   
     
     def attack(self,dinosaur):
-        damage_list =[1,.2,.3,2,1]
-        
-        self.hit_points = random.choice(damage_list)*Weapon.attack_power()
-        print(f"{self.name}used the {self.name}on{dinosaur[0].name()}.")
-
-        dinosaur[0].health=self.hit_points - dinosaur.health()
+       dinosaur.health -= self.weapon.attack_power
         
         
-
-dino = []
-
-
-
-rob =Dinosaur(100,"rob",100)
-bab=Dinosaur(100,"bab",20)
-dino.append(bab)
-dino.append(rob)
-
-wood_axe = Weapon("axe",1)
-
-
-Peter_bot=Robot("Peter",wood_axe,100)
-
-Peter_bot.attack(dino)
